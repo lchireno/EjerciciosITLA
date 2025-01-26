42c6217f85e2c246ef873faad216372f787cac8f
Leonardi Alonzo Chireno <leonardi.chireno@hotmail.com>
2025-01-25 20:21:58 -0400

    se agrego un nuevo ejercicio de una calculadora para multiplicar dos numeros in usar el operador *

---
+++ Multiplicador/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Multiplicador
+{
+    partial class frmMultiplicador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMultiplicador));
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(97, 88);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(135, 22);
+            this.txtNum1.TabIndex = 0;
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(97, 116);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(135, 22);
+            this.txtNum2.TabIndex = 1;
+            this.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMultiplicar.Location = new System.Drawing.Point(97, 172);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(135, 32);
+            this.btnMultiplicar.TabIndex = 3;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Número 2:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(97, 144);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(135, 22);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado:";
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(262, 32);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Multiplicar dos números";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Location = new System.Drawing.Point(0, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Leonardi Alonzo Chireno";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMultiplicador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 232);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMultiplicador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multiplicador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Multiplicador/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplicador
+{
+    public partial class frmMultiplicador : Form
+    {
+        // declaracion de variables 
+        int num1, num2;
+
+        public frmMultiplicador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = Multiplicar();
+        }
+
+        public string Multiplicar() // el metodo sera del tipo booleano para devolver verdadero si se cumple o falso si no
+        {
+            int resultado = 0;
+            // evalua txtNum1, si no está vacio o si no es un nuero, si es un numero lo asigna a la variable num1
+            if (txtNum1.Text == "" || !int.TryParse(txtNum1.Text, out num1))
+            {
+                MessageBox.Show("Introduzca un valor válido.", "Atención"); // muestra un mensaje,
+                txtNum1.Select(); // selecciona el cuadro de texto 
+
+                return ""; //y retorna falso
+            }
+
+            if (txtNum2.Text == "" || !int.TryParse(txtNum2.Text, out num2))
+            {
+                MessageBox.Show("Introduzca un valor válido.", "Atención");
+                txtNum2.Select();
+
+                return "";
+            }
+ 
+            // multiplicacion sin usar el operador *, sumando num2 n veces, donde n es igual a num1
+            for (int i = 1; i <= num1; i++)
+            {
+                resultado += num2;
+            }
+            return resultado.ToString("#,##0.###");
+        }
+
+    }
+}
